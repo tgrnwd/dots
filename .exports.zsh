e0eea9b8ab82da73e0860b7f2c21895203654a14
Tim Greenwood <1031568+tgrnwd@users.noreply.github.com>
2025-02-28 16:50:25 -0600

    add python and java things for now

--- .exports.zsh
+++ .exports.zsh
@@ -6,10 +6,18 @@ if type gh > /dev/null; then
   fi
 fi
 
+#NVM
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+#GO
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+eval "$(pyenv virtualenv-init -)"
